f9780e83f905e50e08b8b97dce217ca08683d24e
AndresOcampo29 <andresocampo1228@gmail.com>
2026-09-22 10:00:16 -0700

    Edit to readme and added analysis.R

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # demo-rstudio-first-ao
 
 This is a demo repo that I created in RStudio first, then sent to github
+
+This is an edit I made in RStudio.
---
+++ analysis.R
@@ -0,0 +1,9 @@
+# View the first few rows of mtcars dataset
+
+head(mtcars)
+
+# COmputing the avg
+mean_mpg <- mean(mtcars$mpg)
+
+# Pinting out
+mean_mpg
